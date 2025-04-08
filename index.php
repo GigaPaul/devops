49b77fd9e3c86bcc1923a42ec57f1e9d7ea63925
GigaPaul <paul.vincent.web@gmail.com>
2025-04-08 13:37:12 +0200

    feat: Ajout Apache + image

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	$title = "Devops Days locations";
+?>
+<img style="max-height:200px" src="./src/images/cesi_logo.webp" alt="">
+<h1><?=$title?></h1>
+<ul>
+	<li>Paris</li>
+	<li>Basele</li>
+</ul>
