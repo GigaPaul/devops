b0c5f6f79b549ecc8958161b4a98a8a1b5db8148
GigaPaul <paul.vincent.web@gmail.com>
2025-04-08 14:02:12 +0200

    Ajout json et tout

--- index.php
+++ index.php
@@ -1,9 +1,32 @@
 <?php
-	$title = "Devops Days locations";
+	$string = file_get_contents("./events.json");
+	$decoded = json_decode($string, true);
+	// var_dump($decoded);
 ?>
-<img style="max-height:200px" src="./src/images/cesi_logo.webp" alt="">
-<h1><?=$title?></h1>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>DevOps</title>
+</head>
+<body>
+
+<h1>Devops Days locations</h1>
 <ul>
-	<li>Paris</li>
-	<li>Basele</li>
+	<?php
+		for($i = 0; $i < count($decoded); $i++) {
+			?>
+			<li>
+				<img style="max-height: 15px" src="./src/images/<?=$decoded[$i]["logo"]?>" alt=""> 
+				<b><?=$decoded[$i]["lieu"]?></b>: 
+				<?=$decoded[$i]["date"]?>
+			</li>
+			<?php
+		}
+	?>
 </ul>
+	
+</body>
+</html>
