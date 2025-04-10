a6d39127f1e3981962bb23d2694c15b8fece7df4
GigaPaul <paul.vincent.web@gmail.com>
2025-04-10 15:01:46 +0200

    Watchtower jsp

--- connect.php
+++ connect.php
@@ -12,7 +12,9 @@ if (!$mysqli || $mysqli->connect_errno) {
     die('<p><strong style="color:red;text-transform:uppercase;">Connection failed: ' . $conn->connect_error . '</strong></p>');
 }
 echo '<p><strong style="color:green;text-transform:uppercase;">Connected successfully!!!!!!</strong></p>';
- 
+ ?>
+    <h1>AAAAAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+ <?php
 // Run query
 $query = "SELECT * FROM country LIMIT 10";
 $result = $mysqli->query($query);
